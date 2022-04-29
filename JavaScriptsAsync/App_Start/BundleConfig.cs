f216882fa24b4c9db326019d2b68ef3110fb38b1
Alfredo Mendiola <Alfredo.Loyola.Globant@kantar.com>
2022-04-29 15:42:42 -0500

    Add Promise Examples

--- JavaScriptsAsync/App_Start/BundleConfig.cs
+++ JavaScriptsAsync/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace JavaScriptsAsync
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/JavaScriptsAsync")
+                .Include("~/Scripts/JavaScriptsAsync.js"));
         }
     }
 }
---
+++ JavaScriptsAsync/Scripts/JavaScriptsAsync.js
@@ -0,0 +1,38 @@
+﻿var url = 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json';
+
+$(document).ready(function ($) {
+    $("#btnSubmit").click(function () {
+        console.log('run');
+        promiseNested();
+    });
+});
+
+function promise01()
+{
+    const fetchPromise = fetch(url);
+
+    console.log(fetchPromise);
+
+    fetchPromise.then(response => {
+        console.log(`Received response: ${response.status}`);
+    });
+
+    console.log("Started request..."); //It is logged before the then callback
+}
+
+function promiseNested() {
+    const fetchPromise = fetch(url);
+
+    console.log(fetchPromise);
+
+    fetchPromise
+        .then(response => {
+            return response.json(); //. json() returns a promise
+        })
+        .then(json => {
+            console.log(json[0].name);
+        });
+
+
+    console.log("Started request..."); //It is logged before the then callback
+}
